177d16c51eac8a5102cab7425de37260c5c13830
almu <muzyka_aleksandr.n@mail.ru>
2018-01-17 22:51:57 +0300

    added singleton pattern example

---
+++ src/singleton/multithread/Main.java
@@ -0,0 +1,33 @@
+package singleton.multithread;
+
+public class Main {
+    public static void main(String ...args) {
+        // All work correct here
+        System.out.println();
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Singleton singleton = Singleton.getInstance("FOO");
+                System.out.println(singleton.value);
+            }
+        });
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Singleton anotherSingleton = Singleton.getInstance("BAR");
+                System.out.println(anotherSingleton.value);
+            }
+        });
+
+        t1.start();
+        t2.start();
+
+        // FOO
+        // FOO
+
+        // or)
+
+        // BAR
+        // BAR
+    }
+}
